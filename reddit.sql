5185c6946bdb85abf4c4d2303508eab1c14f94e3
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-07-20 21:54:02 +0000

    completed subreddit p1

--- reddit.sql
+++ reddit.sql
@@ -13,13 +13,21 @@ CREATE TABLE `users` (
 -- This creates the posts table. The userId column references the id column of
 -- users. If a user is deleted, the corresponding posts' userIds will be set NULL.
 CREATE TABLE `posts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(300) DEFAULT NULL,
-  `url` varchar(2000) DEFAULT NULL,
-  `userId` int(11) DEFAULT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(300) DEFAULT NULL,
+  `url` VARCHAR(2000) DEFAULT NULL,
+  `userId` INT(11) DEFAULT NULL,
   `createdAt` DATETIME NOT NULL,
   `updatedAt` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
   KEY `userId` (`userId`),
   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- CREATE TABLE `subreddits` (
+--   `id` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   `name` VARCHAR(30) NOT NULL UNIQUE KEY,
+--   `description` VARCHAR(200) DEFAULT NULL,
+--   `createdAt` DATETIME NOT NULL,
+--   `updatedAt` DATETIME NOT NULL,
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
